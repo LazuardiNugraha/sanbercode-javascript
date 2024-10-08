184c29bfeb8800050ca112f4ba1f2139c075cc8d
nugrahalazuardim <nugrahalazuardim@gmail.com>
2024-10-08 23:16:44 +0700

    latihan-11_tugas_validation_mongoose_middleware

--- latihan_10/latihan-crud/src/controllers/products.controller.ts
+++ latihan_10/latihan-crud/src/controllers/products.controller.ts
@@ -1,34 +1,80 @@
-import { Request, Response } from "express";
-import ProductsModel from "@/models/products.model";
+import { Request, Response } from 'express';
+import ProductsModel from '@/models/products.model';
+import * as Yup from 'yup';
+
+const createValidationSchema = Yup.object().shape({
+  name: Yup.string().required(),
+  price: Yup.number().required(),
+  category: Yup.string().required(),
+  description: Yup.string().required(),
+  images: Yup.array().of(Yup.string()).required().min(1),
+  qty: Yup.number().required().min(1),
+});
+
+interface IPaginationQuery {
+  page: number;
+  limit: number;
+  search?: string;
+}
 
 export default {
   async create(req: Request, res: Response) {
     try {
+      await createValidationSchema.validate(req.body);
       const result = await ProductsModel.create(req.body);
       res.status(201).json({
         data: result,
-        message: "Success create product",
+        message: 'Success create product',
       });
     } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        res.status(400).json({
+          data: error.errors,
+          message: 'Failed create product',
+        });
+        return;
+      }
+
       const err = error as Error;
       res.status(500).json({
         data: err.message,
-        message: "Failed create product",
+        message: 'Failed create product',
       });
     }
   },
   async findAll(req: Request, res: Response) {
     try {
-      const result = await ProductsModel.find();
+      const { limit = 10, page = 1, search = '' } = req.query as unknown as IPaginationQuery;
+
+      const query = {};
+
+      if (search) {
+        Object.assign(query, {
+          name: { $regex: search, $options: 'i' },
+        });
+      }
+
+      const result = await ProductsModel.find(query)
+        .limit(limit)
+        .skip((page - 1) * limit)
+        .sort({ createdAt: -1 })
+        .populate('category');
+
+      const total = await ProductsModel.countDocuments(query);
+
       res.status(200).json({
         data: result,
-        message: "Success get all products",
+        message: 'Success get all products',
+        page: +page,
+        limit: +limit,
+        total,
+        totalPages: Math.ceil(total / limit)
       });
     } catch (error) {
       const err = error as Error;
       res.status(500).json({
         data: err.message,
-        message: "Failed get all products",
+        message: 'Failed get all products',
       });
     }
   },
@@ -39,35 +85,31 @@ export default {
       });
       res.status(200).json({
         data: result,
-        message: "Success get one product",
+        message: 'Success get one product',
       });
     } catch (error) {
       const err = error as Error;
       res.status(500).json({
         data: err.message,
-        message: "Failed get one product",
+        message: 'Failed get one product',
       });
     }
   },
   async update(req: Request, res: Response) {
     try {
-      const result = await ProductsModel.findOneAndUpdate(
-        { _id: req.params.id },
-        req.body,
-        {
-          new: true,
-        }
-      );
+      const result = await ProductsModel.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+      });
 
       res.status(200).json({
         data: result,
-        message: "Success update product",
+        message: 'Success update product',
       });
     } catch (error) {
       const err = error as Error;
       res.status(500).json({
         data: err.message,
-        message: "Failed update product",
+        message: 'Failed update product',
       });
     }
   },
@@ -79,13 +121,13 @@ export default {
 
       res.status(200).json({
         data: result,
-        message: "Success delete product",
+        message: 'Success delete product',
       });
     } catch (error) {
       const err = error as Error;
       res.status(500).json({
         data: err.message,
-        message: "Failed delete product",
+        message: 'Failed delete product',
       });
     }
   },
