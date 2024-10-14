9865ae0b8eeff5f09408bcb4d556881634c0b01f
nugrahalazuardim <nugrahalazuardim@gmail.com>
2024-10-14 23:11:35 +0700

    latihan-15_tugas_jwt

--- latihan_12/src/routes/api.ts
+++ latihan_12/src/routes/api.ts
@@ -4,6 +4,8 @@ import uploadMiddleware from "../middlewares/upload.middleware";
 import uploadController from "../controllers/upload.controller";
 import productsController from "../controllers/products.controller";
 import categoriesController from "../controllers/categories.controller";
+import authController from '../controllers/auth.controller';
+import authMiddleware from '../middlewares/auth.middlewares';
 
 const router = express.Router();
 
@@ -24,5 +26,10 @@ router.delete("/products/:id", productsController.delete);
 
 router.post("/upload", uploadMiddleware.single, uploadController.single);
 router.post("/uploads", uploadMiddleware.multiple, uploadController.multiple);
+
+router.post('/auth/login', authController.login);
+router.post('/auth/register', authController.register);
+router.post('/auth/me', authMiddleware, authController.me);
+router.put('/auth/update-profile', authMiddleware, authController.profile);
 
 export default router;
--- latihan_12/src/utils/env.ts
+++ latihan_12/src/utils/env.ts
@@ -1,10 +1,9 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || "";
-export const CLOUDINARY_API_SECRET: string =
-  process.env.CLOUDINARY_API_SECRET || "";
-export const CLOUDINARY_CLOUD_NAME: string =
-  process.env.CLOUDINARY_CLOUD_NAME || "";
-export const DATABASE_URL: string = process.env.DATABASE_URL || "";
+export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || '';
+export const CLOUDINARY_API_SECRET: string = process.env.CLOUDINARY_API_SECRET || '';
+export const CLOUDINARY_CLOUD_NAME: string = process.env.CLOUDINARY_CLOUD_NAME || '';
+export const DATABASE_URL: string = process.env.DATABASE_URL || '';
+export const SECRET: string = process.env.SECRET || '249982ed48a26019a534b5449c4addd8';
